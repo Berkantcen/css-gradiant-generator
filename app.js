1b861b46e6e8a5bfc641be68f84a3e9a9eb3193e
Berkant CEN <69425673+Berkantcen@users.noreply.github.com>
2021-10-20 11:17:49 +0300

    clear unnessary code

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2');
-const randomBtn = document.querySelector('.random');
-const body = document.querySelector('body');
-const message = document.getElementById('message');
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const randomBtn = document.querySelector(".random");
+const body = document.querySelector("body");
+const message = document.getElementById("message");
 const hexcodes = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
 ];
 
-input1.addEventListener('input', function () {
+// EVENTS
+
+input1.addEventListener("input", function () {
   let firstColor = input1.value;
   body.style.background = `linear-gradient(90deg, ${firstColor}, ${input2.value})`;
   updateColorMessage();
 });
 
-input2.addEventListener('input', function () {
+input2.addEventListener("input", function () {
   let secondColor = input2.value;
   body.style.background = `linear-gradient(90deg, ${input1.value}, ${secondColor})`;
   updateColorMessage();
 });
 
+randomBtn.addEventListener("click", function () {
+  applyRandomColor();
+});
+
+// CUSTOM FUNCTION
 function updateColorMessage() {
   message.innerText = `background : linear-gradient(90deg, ${input1.value}, ${input2.value})`;
 }
 
-updateColorMessage();
-applyRandomColor();
-
-randomBtn.addEventListener('click', function () {
-  applyRandomColor();
-});
-
 function getRandomColor() {
-  let randomColor = '#';
+  let randomColor = "#";
   while (randomColor.length < 7) {
     randomColor =
       randomColor + hexcodes[Math.floor(Math.random() * hexcodes.length)];
@@ -55,7 +55,7 @@ function getRandomColor() {
 }
 
 function applyRandomColor() {
-  let randomColor = '';
+  let randomColor = "";
   randomColor = getRandomColor();
   let firstRandom = getRandomColor();
   let secondRandom = getRandomColor();
@@ -66,3 +66,7 @@ function applyRandomColor() {
   input2.value = secondRandom;
   updateColorMessage();
 }
+
+// RUN AT LOAD
+updateColorMessage();
+applyRandomColor();
